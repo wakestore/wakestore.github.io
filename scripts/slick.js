c1459e9ed0b25fa71548ff495c636bb7115e7cfa
Luiz <luizhc2309@hotmail.com>
2023-04-17 09:38:51 -0300

    correção de script

---
+++ scripts/slick.js
@@ -0,0 +1,65 @@
+$('.sponsor-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+jQuery(function() {
+jQuery(".step-carousel").slick({
+    lazyLoad: 'ondemand',
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+        {
+        breakpoint: 1300,
+        settings: {
+            slidesToShow: 3,
+        },
+        },
+        {
+        breakpoint: 1000,
+        settings: {
+            slidesToShow: 2,
+        },
+        },
+        {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+        },
+        },
+    ],
+})
+})
+
+$('.step-carousel__item').click(function(){
+$('.step-carousel').slick('slickNext'); // avança para o próximo item do Slick
+});
+
+window.addEventListener('load',function(){var minhaDiv=document.querySelector('.step');minhaDiv.classList.add('mostrar');});
